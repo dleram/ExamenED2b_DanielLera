982074f90c1e871f74fa03fe62e49cfae9214637
dleram <dleram11@gmail.com>
2023-03-31 09:12:32 +0200

    Proyecto sin documentar

---
+++ src/Hora.java
@@ -0,0 +1,19 @@
+package examen;
+
+public class Hora {
+	public static boolean validarHora(int horas, int minutos, int segundos) {
+		boolean resultado = true;
+		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
+			if (horas > 23) {
+				resultado = false;
+			} else if (minutos > 59) {
+				resultado = false;
+			} else if (segundos > 59) {
+				resultado = false;
+			}
+		} else {
+			resultado = false;
+		}
+		return resultado;
+	}
+}
